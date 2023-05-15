ef35dc20e5c0ef6799dcb737e7cbfbf9437a755e
choiruladamm <choiruladamm@gmail.com>
2023-05-15 18:45:02 +0700

    Add Nested Routing

---
+++ app/posts/new/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// /posts/new
+
+const page = () => {
+  return (
+    <div>NEW POST</div>
+  )
+}
+
+export default page
---
+++ app/posts/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// /posts
+
+const page = () => {
+  return (
+    <div>POST</div>
+  )
+}
+
+export default page
