5f1abf4ed1e3dd5acef65c62cadac9e3fc73cb02
choiruladamm <choiruladamm@gmail.com>
2023-05-15 20:34:18 +0700

    NextJS API Endpoint

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,3 +1,12 @@
 export async function GET(request) {
-  return new Response('Hello Nextjs')
+  // handle GET request for /api/user
+  // retrieve user users from the database or any data database
+  const users = [
+    {id: 1, name: 'John'},
+    {id: 2, name: 'Jane'},
+    {id: 3, name: 'Bob'},
+  ]
+
+  return new Response(JSON.stringify(users))
+
 }
