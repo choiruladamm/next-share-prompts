3a7d3516e73580697ea15d3db35b7173dc8b9713
choiruladamm <choiruladamm@gmail.com>
2023-05-25 22:26:32 +0700

    Add Session Provider

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,6 +1,12 @@
-const Provider = () => {
+'use client'
+
+import { SessionProvider } from "next-auth/react";
+
+const Provider = ({ children, session }) => {
   return (
-    <div>Provider</div>
+   <SessionProvider session={session}>
+    { children }
+   </SessionProvider>
   )
 }
 
