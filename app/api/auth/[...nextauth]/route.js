9339d56d0f0da951d4bee8a91ece5cfd3ad8f1bd
choiruladamm <choiruladamm@gmail.com>
2023-05-25 22:41:15 +0700

    Add Google Providers

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { signIn } from "next-auth/react";
 
 const handler = NextAuth({
   providers: [
@@ -8,4 +9,12 @@ const handler = NextAuth({
       clientSecret: "",
     }),
   ],
+  async session({ session }) {
+
+  },
+  async signIn({ profile }) {
+
+  }
 });
+
+export { handler as GET, handler as POST} 
