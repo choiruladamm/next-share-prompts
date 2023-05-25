77fde55dd81c207e74b89b7a3ede93b2911b6678
choiruladamm <choiruladamm@gmail.com>
2023-05-25 22:29:01 +0700

    Add Session Provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "@styles/globals.css";
+
 import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
@@ -11,14 +12,16 @@ const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-        <main className="app">
-          <Nav />
-          { children }
-        </main>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
