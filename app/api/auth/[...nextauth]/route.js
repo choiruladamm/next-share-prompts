3f12075e7dc7732e25580a1df916cdc1a4eadc9a
choiruladamm <choiruladamm@gmail.com>
2023-05-27 19:57:50 +0700

    Add Google Providers

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -7,10 +7,7 @@ import { connectToDB } from "@utils/database";
 
 const handler = NextAuth({
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
+    
   ],
   async session({ session }) {
 
