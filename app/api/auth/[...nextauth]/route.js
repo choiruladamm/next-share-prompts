ec48c0264b207c3c030822378ea635bb42480d91
choiruladamm <choiruladamm@gmail.com>
2023-05-28 23:30:05 +0700

    Fix Store MongoDB

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -12,38 +12,40 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    // store the user id from MongoDB to session
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
-    session.user.id = sessionUser._id.toString();
-
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
-
-      // check if users is already exist
-      const userExist = await User.findOne({
-        email: profile.email,
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({
+        email: session.user.email,
       });
+      session.user.id = sessionUser._id.toString();
 
-      // if not, create a new user
-      if (!userExist) {
-        await User.create({
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+
+        // check if users is already exist
+        const userExist = await User.findOne({
           email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
         });
-      }
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+        // if not, create a new user
+        if (!userExist) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
 });
 
